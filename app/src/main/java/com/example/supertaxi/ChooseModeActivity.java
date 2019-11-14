2e74f43870a73a08528e6567f7c7a1863b0ff607
DNS <Nurlan_dzhumabaev@mail.ru>
2019-11-14 13:25:22 +0600

    Splash скрины

---
+++ app/src/main/java/com/example/supertaxi/ChooseModeActivity.java
@@ -0,0 +1,23 @@
+package com.example.supertaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
+
+    }
+
+    public void goToDriverSignIn(View view) {
+
+    }
+}
